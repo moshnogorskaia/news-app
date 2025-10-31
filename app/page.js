e419dc69bb6becb23df0af606d7fb1a874677708
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-10-31 21:52:06 +0100

    Add main header

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
+import MainHeader from '@/components/main-header';
+import Link from 'next/link';
+
 export default function HomePage() {
   return (
     <div id="home">
+      <MainHeader />
       <h1>Next.js Routing & Page Rendering</h1>
     </div>
   );
