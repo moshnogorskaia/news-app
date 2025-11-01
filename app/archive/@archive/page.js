7beed91e63cd45c66d6a5efb0c7177b773271c4b
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-01 10:49:33 +0100

    Add a dynamic page for years and news utilities

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,7 +1,16 @@
 export default function ArchivePage() {
     return (
-        <div>
-            <h1>Archive Page</h1>
-        </div>
+        <header id="archive-header">
+            <nav>
+            <ul>
+                <li>
+                    <Link href="/archive/2025">2025</Link>
+                </li>
+                <li>
+                    <Link href="/archive/2024">2024</Link>
+                </li>
+            </ul>
+            </nav>
+        </header>
     )
 }
