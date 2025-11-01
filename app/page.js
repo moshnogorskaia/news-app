f889f34efd904d526d9b1e3c2b3081bd21a3adc8
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-01 10:06:38 +0100

    Move the header into layout

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
-import MainHeader from '@/components/main-header';
 import Link from 'next/link';
 
 export default function HomePage() {
   return (
     <div id="home">
-      <MainHeader />
       <h1>Next.js Routing & Page Rendering</h1>
     </div>
   );
--- components/main-header.js
+++ components/main-header.js
@@ -3,8 +3,14 @@ import Link from 'next/link';
 export default function MainHeader() {
     return (
         <header>
-            <Link href="/">Home</Link>
-            <Link href="/news">News</Link>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/news">News</Link>
+                </li>
+            </ul>
         </header>
     )
 }
