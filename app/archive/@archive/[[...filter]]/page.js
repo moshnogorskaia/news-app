8251998d9e3eb42845b164721fe086e236e80499
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-04 20:04:19 +0100

    Show content in catch-all page

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,21 +1,41 @@
 import NewsList from '@/components/news-list';
-import { getAvailableNewsYears } from '@/lib/news';
+import { getAvailableNewsYears, getNewsForYear } from '@/lib/news';
 import Link from 'next/link';
 
-export default function FilteredNewsPage ({ params }) {
-    const { filter } = params;
+export default function FilteredNewsPage({ params }) {
+  const { filter } = params;
 
-    const links = getAvailableNewsYears(); 
+  const links = getAvailableNewsYears();
 
-    return ( <header id="archive-header">
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+  }
+
+  let newsContent = <p>No news found for selected period</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  return (
+    <>
+      <header id="archive-header">
         <nav>
-        <ul>
-               {links.map((year) => (
-                <li key={year}>
-                    <Link href={`/archive/${year}`}>{year}</Link>
-                </li>
-               ))}
-        </ul>
+          <ul>
+            {links.map((year) => (
+              <li key={year}>
+                <Link href={`/archive/${year}`}>{year}</Link>
+              </li>
+            ))}
+          </ul>
         </nav>
-    </header>)
+      </header>
+      {newsContent}
+    </>
+  );
 }
