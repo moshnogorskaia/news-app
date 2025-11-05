23714e4ff363f73f560351bd6b715fc3d688b09b
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-05 21:54:57 +0100

    Close the Modal on click on backdrop

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,8 +1,12 @@
+'use client';
+
 import { DUMMY_NEWS } from '@/dummy-news';
 import { notFound } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function ImagePage({ params }) {
   const { slug } = params;
+  const router = useRouter();
 
   const newsItem = DUMMY_NEWS.find((news) => news.slug === slug);
 
@@ -12,7 +16,7 @@ export default function ImagePage({ params }) {
 
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fulscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
