f73d0e2dc737405654c14e4e3bcb3050ccf9a54a
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-05 22:24:51 +0100

    Add API with route handlers

---
+++ app/api/route.js
@@ -0,0 +1,16 @@
+export function GET(request) {
+  console.log(request);
+  return new Response('Hello, world!');
+}
+
+// export function POST(request) {
+//     return new Response('Hello, world!');
+// }
+
+// export function PUT(request) {
+//     return new Response('Hello, world!');
+// }
+
+// export function DELETE(request) {
+//     return new Response('Hello, world!');
+// }
