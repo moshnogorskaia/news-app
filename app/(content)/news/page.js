a00d96e6ee4dc0c050ea2351892bbd6607612a49
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-07 13:18:57 +0100

    Add loading fallback

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,8 +1,8 @@
 import NewsList from '@/components/news-list';
 import { getAllNews } from '@/lib/news';
 
-export default function NewsPage() {
-  const news = getAllNews();
+export default async function NewsPage() {
+  const news = await getAllNews();
 
   return (
     <>
